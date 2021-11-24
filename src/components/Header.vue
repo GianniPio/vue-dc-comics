00642a90c9bd6185bddce43003cd1ee69adbc523
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-24 14:39:42 +0100

    add new component header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="header">
+    ciao
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    #header {
+        background-color: red;
+        height: 15vh;
+        width: 100%;
+    }
+</style>
